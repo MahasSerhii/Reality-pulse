9f2ca8cf8f71cbc554682759edec44aa75c74fb3
Serhii_Mahas <serhii.mahas@evlue.cz>
2024-03-15 11:29:14 +0100

    Added home page and properties page

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import "@/assets/styles/globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 export const metadata = {
-  title: "PropertyPulse | Find The Perfect Rental",
+  title: "PropertyPulse | Find The Perfect Rental", // used in any page to provide SEO, now it setting up here as global
   description: "Find your dream rental property",
   keywords: "rental, find rentals, find properties",
 };
@@ -10,7 +12,9 @@ const MainLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div>{children}</div>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
